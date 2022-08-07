cbee5ec39f3f885c891fedf92529e2a41ab8d1af
Bunyameen <bunyameen@gmail.com>
2022-08-07 11:41:46 +0100

    final Example

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import data from './data.json'
+
 
 import './App.css';
 
@@ -5,9 +7,33 @@ function App() {
   return (
     <div className="App">
       <h1>Student Records</h1>
+
+      {
+        data.map((datum) => {
+          return(
+            <div key={datum.id}>
+              
+              {
+              datum.name &&  datum.name.map((name) =>{
+                 return(
+                  <span key={datum.id}>{name.firstName} {name.lastName}</span>
+
+                 ) 
+                })
+                
+                
+              }
+              <img src={datum.image && datum.image.small} alt="img" />
+              <p>{datum.class && datum.class.level}</p>
+            </div>
+          )
+        })
+      }
       
     </div>
   );
 }
 
 export default App;
+
+
